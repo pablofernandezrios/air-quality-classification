c839be5bde4a4c38c07c41d4a43d5b5b23aac164
paularodil <paula.rodil@udc.es>
2026-03-08 11:37:01 +0100

    feat: revisa el apartado 4.1 de matrices de confusión

--- ejercicio4.jl
+++ ejercicio4.jl
@@ -6,37 +6,98 @@
 # ----------------------------------------------------------------------------------------------
 
 
-function confusionMatrix(outputs::AbstractArray{Bool,1}, targets::AbstractArray{Bool,1})
+function confusionMatrix(ouVPuts::AbstractArray{Bool,1}, targets::AbstractArray{Bool,1})
+
+    @assert length(ouVPuts) == length(targets) "Los vectores ouVPuts y targets deben tener la misma longitud"
+
+    # calculamos los valores de la matriz de confusión
+    VP = sum(ouVPuts .& targets)
+    VN = sum(.!ouVPuts .& .!targets)
+    FP = sum(ouVPuts .& .!targets)
+    FN = sum(.!ouVPuts .& targets)
+
+    # calculamos las métricas
+    accuracy = (VN + VP) / (VN + VP + FN + FP)
+    errorrate = (FN + FP) / (VN + VP + FN + FP)
+    sensitivity = (VP + FN) == 0 ? 1.0 : VP / (VP + FN)
+    specificity = (VN + FP) == 0 ? 1.0 : VN / (VN + FP)
+    ppv = (VP + FP) == 0 ? 1.0 : VP / (VP + FP)
+    npv = (VN + FN) == 0 ? 1.0 : VN / (VN + FN)
+    f1 = (ppv + sensitivity) == 0 ? 0.0 :
+        2 * (ppv * sensitivity) / (ppv + sensitivity)
+
+    confusionmatrix = [VN FP;
+                        FN VP]
+
+    return (accuracy, errorrate, sensitivity, specificity, ppv, npv, f1, confusionmatrix)
+
+end;
+
+function confusionMatrix(ouVPuts::AbstractArray{<:Real,1}, targets::AbstractArray{Bool,1}; threshold::Real=0.5)
+
+    @assert length(ouVPuts) == length(targets) "Los vectores ouVPuts y targets deben tener la misma longitud"
+
+    ouVPuts_bool = ouVPuts .>= threshold  # comprueba si cada uno de los valores del output es mayor que el umbral (True)
+    return confusionMatrix(ouVPuts_bool, targets)
+
+end;
+
+function printConfusionMatrix(outputs::AbstractArray{Bool,1}, targets::AbstractArray{Bool,1}) 
+
+    metricas = confusionMatrix(outputs, targets)
+
+    println("Métricas:")
+    println("Accuracy: $(metricas[1])")
+    println("Error rate: $(metricas[2])")
+    println("Sensitivity: $(metricas[3])")
+    println("Specificity: $(metricas[4])")
+    println("PPV: $(metricas[5])")
+    println("NPV: $(metricas[6])")
+    println("F1-score: $(metricas[7])")
+
+    println("\nMatriz de confusión:")
+    println(metricas[8])
+
+end;
+    
+    
+function printConfusionMatrix(outputs::AbstractArray{<:Real,1}, targets::AbstractArray{Bool,1}; threshold::Real=0.5)
+
+    metricas = confusionMatrix(outputs, targets, threshold)
+
+    println("Métricas:")
+    println("Accuracy: $(metricas[1])")
+    println("Error rate: $(metricas[2])")
+    println("Sensitivity: $(metricas[3])")
+    println("Specificity: $(metricas[4])")
+    println("PPV: $(metricas[5])")
+    println("NPV: $(metricas[6])")
+    println("F1-score: $(metricas[7])")
+
+    println("\nMatriz de confusión:")
+    println(metricas[8])
+
+end;
+
+function confusionMatrix(ouVPuts::AbstractArray{Bool,2}, targets::AbstractArray{Bool,2}; weighted::Bool=true)
     #
     # Codigo a desarrollar
     #
 end;
 
-function confusionMatrix(outputs::AbstractArray{<:Real,1}, targets::AbstractArray{Bool,1}; threshold::Real=0.5)
+function confusionMatrix(ouVPuts::AbstractArray{<:Real,2}, targets::AbstractArray{Bool,2}; threshold::Real=0.5, weighted::Bool=true)
     #
     # Codigo a desarrollar
     #
 end;
 
-function confusionMatrix(outputs::AbstractArray{Bool,2}, targets::AbstractArray{Bool,2}; weighted::Bool=true)
+function confusionMatrix(ouVPuts::AbstractArray{<:Any,1}, targets::AbstractArray{<:Any,1}, classes::AbstractArray{<:Any,1}; weighted::Bool=true)
     #
     # Codigo a desarrollar
     #
 end;
 
-function confusionMatrix(outputs::AbstractArray{<:Real,2}, targets::AbstractArray{Bool,2}; threshold::Real=0.5, weighted::Bool=true)
-    #
-    # Codigo a desarrollar
-    #
-end;
-
-function confusionMatrix(outputs::AbstractArray{<:Any,1}, targets::AbstractArray{<:Any,1}, classes::AbstractArray{<:Any,1}; weighted::Bool=true)
-    #
-    # Codigo a desarrollar
-    #
-end;
-
-function confusionMatrix(outputs::AbstractArray{<:Any,1}, targets::AbstractArray{<:Any,1}; weighted::Bool=true)
+function confusionMatrix(ouVPuts::AbstractArray{<:Any,1}, targets::AbstractArray{<:Any,1}; weighted::Bool=true)
     #
     # Codigo a desarrollar
     #
